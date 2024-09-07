5bc438c1ef807f63814453fbe35deb37b6346e7b
Mahmud Lukman <mistamann49@gmail.com>
2024-09-07 14:21:11 +0100

    categories page added

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import Header from "../components/Layout/Header";
 import Hero from "../components/Route/Hero/Hero";
-// import Categories from "../components/Route/Categories/Categories";
+import Categories from "../components/Route/Categories/Categories";
 // import BestDeals from "../components/Route/BestDeals/BestDeals";
 // import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct";
 // import Events from "../components/Events/Events";
@@ -12,7 +12,7 @@ const HomePage = () => {
     <div>
         <Header activeHeading={1} />
         <Hero />
-        {/* <Categories /> */}
+        <Categories />
         {/* <BestDeals /> */}
         {/* <Events /> */}
         {/* <FeaturedProduct /> */}
